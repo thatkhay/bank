9b7eecabfab58e296f0eaffcee54c537ce4b0853
thatkhay <johnkingsleyegeonu@gmail.com>
2024-01-20 14:17:58 +0100

    loader edit

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Loader = () => {
   return (
      <div className="flex items-center justify-center h-[4rem] mt-4">
-      <div className="border-t-8 border-blue-500 border-solid rounded-full animate-spin h-16 w-16"></div>
+      <div className="border-t-8 border-blue-500 border-solid rounded-full animate-spin h-10 w-10"></div>
     </div>
   )
 }
