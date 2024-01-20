39e6dfd66282ae8c1ada06393120701abbd73014
thatkhay <johnkingsleyegeonu@gmail.com>
2024-01-20 11:41:11 +0100

    visibiity

--- src/pages/UserTransactionPage.jsx
+++ src/pages/UserTransactionPage.jsx
@@ -1,24 +1,30 @@
 import React, { useEffect, useState } from "react";
 import logo from "../images/citi-logo.svg";
 import { Link } from "react-router-dom";
-import AddCardIcon from '@mui/icons-material/AddCard';
-import SendIcon from '@mui/icons-material/Send';
+import AddCardIcon from "@mui/icons-material/AddCard";
+import SendIcon from "@mui/icons-material/Send";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getFirestore, doc, getDoc } from "firebase/firestore";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import VisibilityIcon from "@mui/icons-material/Visibility";
 
 const UserTransactionPage = () => {
-  const [userName, setUserName] = useState('');
+  const [userName, setUserName] = useState("");
   const [displayNumber, setDisplayNumber] = useState(null);
-
+  const [visible, setVisible] = useState(true);
   const auth = getAuth();
   const firestore = getFirestore();
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
         const user = auth.currentUser;
         if (user) {
-          setUserName(user.displayName || '');
+          setUserName(user.displayName || "");
 
           const userDocRef = doc(firestore, "users", user.uid);
           const userDocSnap = await getDoc(userDocRef);
@@ -56,16 +62,31 @@ const UserTransactionPage = () => {
       </div>
       <div className="lg:w-[50%] md:w-[70%] sm:w-[80%] xsm:w-[80%] h-[10rem] bg-blue-600 px-20 py-10 flex flex-col items-center justify-center gap-2 rounded-lg mt-6">
         <p className="font-semibold text-slate-400">Total Savings</p>
-        <p className="text-[2rem] text-white tracking-wider font-semibold">
-          ${displayNumber}
-        </p>
+        <div className="flex items-center ">
+          <p className="text-[2rem] text-white tracking-wider font-semibold">
+            <div className="ml-[2rem]">
+              {visible
+                ? // Display the number if visible is true
+                  `$${displayNumber}`
+                : // Display asterisks if visible is false
+                  "********"}
+            </div>
+          </p>
+          {visible ? (
+            // Show the eye icon if visible is true
+            <VisibilityIcon onClick={toggleVisibility} />
+          ) : (
+            // Show the crossed eye icon if visible is false
+            <VisibilityOffIcon onClick={toggleVisibility} />
+          )}
+        </div>
       </div>
       <div className="flex items-center justify-around lg:w-[50%] md:w-[70%] sm:w-[90%] xsm:w-[90%] pt-4">
         <div className="flex items-center justify-around h-[3rem] w-[10rem] bg-blue-400 rounded-md">
-          Add Funds <AddCardIcon/>
+          Add Funds <AddCardIcon />
         </div>
         <div className="flex items-center justify-around h-[3rem] w-[10rem] bg-green-400 rounded-md">
-          Transfer Funds <SendIcon/>
+          Transfer Funds <SendIcon />
         </div>
       </div>
       <div className="w-[90%] h-[10rem] border-2 rounded-md mt-4"></div>
